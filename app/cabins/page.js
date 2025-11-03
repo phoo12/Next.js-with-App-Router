e749cd27f62d90aa7e0290cfd642e29ee27eb0c4
khaingphoowai <khaingphoowaikw@gmail.com>
2025-11-03 10:03:55 +0700

    creating supabase key&url/getting API,use Image with supabase,creating loading&spinner,using suspense in next.js

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,12 +1,13 @@
-import Counter from "@/app/_components/Counter";
-import CabinCard from "@/app/_components/CabinCard";
+import { Suspense } from "react";
+import CabinList from "../_components/CabinList";
+import Spinner from "../_components/Spinner";
 
 export const metadata = {
   title: "Cabins",
 };
 export default function Page() {
   // CHANGE
-  const cabins = [];
+  // console.log("Starting...");
 
   return (
     <div>
@@ -22,13 +23,9 @@ export default function Page() {
         Welcome to paradise.
       </p>
 
-      {cabins.length > 0 && (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-          {cabins.map((cabin) => (
-            <CabinCard cabin={cabin} key={cabin.id} />
-          ))}
-        </div>
-      )}
+      <Suspense fallback={<Spinner />}>
+        <CabinList />
+      </Suspense>
     </div>
   );
 }
