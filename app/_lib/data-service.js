075b3ec8fe90fab07dffa2108d180dccfa58c0d3
khaingphoowai <khaingphoowaikw@gmail.com>
2025-11-04 09:51:23 +0700

    sharing state betweeen client & server:url (client navigation)

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -42,7 +42,7 @@ export const getCabins = async function () {
     .select("id, name, maxCapacity, regularPrice, discount, image")
     .order("name");
   // For testing
-  await new Promise((res) => setTimeout(res, 1000));
+  // await new Promise((res) => setTimeout(res, 500));
   if (error) {
     console.error(error);
     throw new Error("Cabins could not be loaded");
